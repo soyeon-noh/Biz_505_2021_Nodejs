0dba7ee7ad33642b492760d544a6ebfb152759cb
soyeon-noh <1010qwerty12@gmail.com>
2021-08-24 16:45:03 +0900

    게시판 수정

--- nodejs_005/routes/bbsRouter.js
+++ nodejs_005/routes/bbsRouter.js
@@ -23,4 +23,56 @@ router.post("/write", (req, res) => {
   tbl_bbs.create(req.body).then((result) => res.redirect("/"));
 });
 
+router.get("/detail", (req, res) => {
+  // list에서 게시물을 클릭했을때
+  // 게시물의 id(b_id)값을 queryString으로 가지고
+  // 여기에 도달한다.
+  const b_id = req.query.b_id;
+
+  // PK를 기준으로 1개의 데이터를 추출하라
+  tbl_bbs.findByPk(b_id).then((result) => {
+    console.table(result);
+    // res.json(result); // 데이터 확인해보기
+    res.render("detail", { BBS: result });
+  });
+});
+
+router.get("/delete", (req, res) => {
+  const b_id = req.query.b_id;
+  tbl_bbs
+    // 2. 데이터를 삭제하라
+    .destroy({
+      /* where를 통해 조건문을 설정하는 것 */
+      // 1. b_id 칼럼의 값이 변수 b_id에 담긴 값과 같으면
+      /* where: { b_id : b_id }, // 원래 이렇게 변수이름과 데이터를 둘다 넣는데 */
+      where: { b_id } /* 둘다같으면 한번만 써도된다.*/,
+    })
+    .then(() => {
+      res.redirect("/");
+    });
+});
+
+router.get("/update", (req, res) => {
+  const b_id = req.query.b_id;
+
+  // PK 또는 일반 칼럼에 조건을 주어 1개의 데이터를 SELECT할때
+  tbl_bbs.findOne({
+    where: { b_id }, // PK 칼럼이 아닌 경우 여러가지 값이 선택될때 오류가 날 수도있다.
+  });
+
+  tbl_bbs.findByPk(b_id).then((result) => {
+    res.render("write", { BBS: result });
+  });
+});
+
+router.post("/update", (req, res) => {
+  const b_id = req.query.b_id;
+
+  // body에 b_id를 추가한다( 원래 없었어도! )
+  req.body.b_id = b_id;
+  tbl_bbs.update(req.body, { where: { b_id } }).then((result) => {
+    res.redirect("/");
+  });
+});
+
 module.exports = router;
